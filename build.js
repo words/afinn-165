3b5f0d19eb390f97b807062f4e6a00700b18cb4c
Titus Wormer <tituswormer@gmail.com>
2021-04-05 15:41:45 +0200

    Update build script

--- build.js
+++ build.js
@@ -2,10 +2,8 @@
 
 var fs = require('fs')
 var https = require('https')
-var Transform = require('readable-stream').Transform
-var csv = require('csv-streamify')
-var wrap = require('wrap-stream')
-var join = require('join-stream')
+var dsv = require('d3-dsv')
+var concat = require('concat-stream')
 var bail = require('bail')
 
 var endpoint =
@@ -13,27 +11,22 @@ var endpoint =
 
 https.get(
   endpoint,
-  {
-    headers: {
-      'User-Agent': 'request',
-      Accept: 'application/vnd.github.v3.raw'
-    }
-  },
+  {headers: {'User-Agent': 'request', Accept: 'application/vnd.github.v3.raw'}},
   onresponse
 )
 
 function onresponse(response) {
-  response
-    .resume()
-    .on('error', bail)
-    .pipe(csv({delimiter: '\t', objectMode: true}))
-    .pipe(new Transform({objectMode: true, transform: onrow}))
-    .pipe(join(',\n'))
-    .pipe(wrap('{\n', '\n}\n'))
-    .pipe(fs.createWriteStream('index.json'))
+  response.pipe(concat(onconcat))
 }
 
-function onrow(chunk, encoding, callback) {
-  this.push('  "' + chunk[0] + '": ' + chunk[1])
-  callback()
+function onconcat(buf) {
+  var data = {}
+  var rows = dsv.tsvParse('key\tvalue\n' + String(buf))
+  var index = -1
+
+  while (++index < rows.length) {
+    data[rows[index].key] = Number.parseInt(rows[index].value, 10)
+  }
+
+  fs.writeFile('index.json', JSON.stringify(data, null, 2) + '\n', bail)
 }
