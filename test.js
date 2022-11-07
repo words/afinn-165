41cdf22ec2be50691dde0c00f2e8f80bd32d4dd0
Titus Wormer <tituswormer@gmail.com>
2022-11-07 11:20:08 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {afinn165} from './index.js'
 
-test('afinn', (t) => {
-  t.equal(afinn165.positive, 2)
-  t.equal(afinn165['self-deluded'], -2)
-  t.equal(afinn165.damn, -2)
-  t.equal(afinn165.futile, -2)
-
-  t.end()
+test('afinn', () => {
+  assert.equal(afinn165.positive, 2)
+  assert.equal(afinn165['self-deluded'], -2)
+  assert.equal(afinn165.damn, -2)
+  assert.equal(afinn165.futile, -2)
 })
