f883aaac2e3988d4cbff23b0a0c722c3c2dcd72b
Titus Wormer <tituswormer@gmail.com>
2022-11-07 11:25:03 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,48 +1,31 @@
-import fs from 'node:fs'
-import https from 'node:https'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
 import {tsvParse} from 'd3-dsv'
-import concat from 'concat-stream'
-import {bail} from 'bail'
 
-const endpoint =
-  'https://api.github.com/repos/fnielsen/afinn/contents/afinn/data/AFINN-en-165.txt'
-
-https.get(
-  endpoint,
-  {headers: {'User-Agent': 'request', Accept: 'application/vnd.github.v3.raw'}},
-  onresponse
+const response = await fetch(
+  'https://api.github.com/repos/fnielsen/afinn/contents/afinn/data/AFINN-en-165.txt',
+  {headers: {'User-Agent': 'request', Accept: 'application/vnd.github.v3.raw'}}
 )
 
-/**
- * @param {import('http').IncomingMessage} response
- */
-function onresponse(response) {
-  response.pipe(concat(onconcat))
+const text = await response.text()
+
+/** @type {Record<string, number>} */
+const data = {}
+const rows = tsvParse('key\tvalue\n' + text)
+let index = -1
+
+while (++index < rows.length) {
+  const {key, value} = rows[index]
+  if (!key) throw new Error('Expected key in `' + rows[index] + '`')
+  if (!value) throw new Error('Expected value in `' + rows[index] + '`')
+  data[key] = Number.parseInt(value, 10)
 }
 
-/**
- * @param {Buffer} buf
- */
-function onconcat(buf) {
-  /** @type {Record<string, number>} */
-  const data = {}
-  const rows = tsvParse('key\tvalue\n' + String(buf))
-  let index = -1
-
-  while (++index < rows.length) {
-    const {key, value} = rows[index]
-    if (!key) throw new Error('Expected key in `' + rows[index] + '`')
-    if (!value) throw new Error('Expected value in `' + rows[index] + '`')
-    data[key] = Number.parseInt(value, 10)
-  }
-
-  fs.writeFile(
-    'index.js',
-    [
-      '/** @type {Record<string, number>} */',
-      'export const afinn165 = ' + JSON.stringify(data, null, 2),
-      ''
-    ].join('\n'),
-    bail
-  )
-}
+await fs.writeFile(
+  'index.js',
+  [
+    '/** @type {Record<string, number>} */',
+    'export const afinn165 = ' + JSON.stringify(data, null, 2),
+    ''
+  ].join('\n')
+)
